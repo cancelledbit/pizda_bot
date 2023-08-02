812d3eb57bf90471099f7c65eb5ef265a27c24ae
Nikita Vasilevskiy <nvasilevskiy@zoon.ru>
2023-08-02 17:51:51 +0300

    several phrases

--- app/repository/wisdom_phrases_repository.go
+++ app/repository/wisdom_phrases_repository.go
@@ -17,18 +17,30 @@ func UserWisdomRepository(ctx context.Context, db *sql.DB) *MysqlUserWisdomRepos
 	}
 }
 
-func (r MysqlUserWisdomRepository) Get(authorId string) (*WisdomPhrase, error) {
-	query := "SELECT id, text, author_id FROM wisdom_phrases where author_id = ? ORDER BY RAND() LIMIT 1;"
+func (r MysqlUserWisdomRepository) Get(authorId string, count int) ([]*WisdomPhrase, error) {
+	query := "SELECT id, text, author_id FROM wisdom_phrases where author_id = ? ORDER BY RAND() LIMIT ?;"
 
 	ctx, cancel := context.WithTimeout(r.ctx, 15*time.Second)
 	defer cancel()
 
-	var phrase WisdomPhrase
-	err := r.fetchRow(r.db.QueryRowContext(ctx, query, authorId), &phrase)
+	rows, err := r.db.QueryContext(ctx, query, authorId, count)
 	if err != nil {
 		return nil, err
 	}
-	return &phrase, nil
+
+	phrases := make([]*WisdomPhrase, 0)
+	for rows.Next() {
+		var p WisdomPhrase
+		err := r.fetchRows(rows, &p)
+		if err != nil {
+			return nil, err
+		}
+		phrases = append(phrases, &p)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return phrases, nil
 }
 
 func (r MysqlUserWisdomRepository) Create(phrase *WisdomPhrase) (*WisdomPhrase, error) {
@@ -52,6 +64,14 @@ func (r MysqlUserWisdomRepository) Create(phrase *WisdomPhrase) (*WisdomPhrase, 
 }
 
 func (r *MysqlUserWisdomRepository) fetchRow(row *sql.Row, phrase *WisdomPhrase) error {
+	return row.Scan(
+		&phrase.ID,
+		&phrase.Text,
+		&phrase.AuthorId,
+	)
+}
+
+func (r *MysqlUserWisdomRepository) fetchRows(row *sql.Rows, phrase *WisdomPhrase) error {
 	return row.Scan(
 		&phrase.ID,
 		&phrase.Text,
